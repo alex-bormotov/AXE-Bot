d487fc86fe67733dcf19b01c8b7db1b036e5592f
Alex Bormotov <31304073+alex-bormotov@users.noreply.github.com>
2019-11-26 22:18:40 +0300

    fix check price

--- market_data.py
+++ market_data.py
@@ -1,4 +1,3 @@
-import ccxt
 import time
 import json
 import requests
@@ -6,22 +5,46 @@ import pandas as pd
 import datetime as dt
 from time import sleep
 from datetime import timedelta
-from exchange import exchange
+
+# from exchange import exchange
 from notification import notificator
 from requests.adapters import HTTPAdapter
 
 show_error = "YES"
-exchange = exchange()
+# exchange = exchange()
+
+
+# def check_coin_price(coin_pair):
+#     try:
+#         return exchange.fetch_ticker(coin_pair)["last"]
+#
+#     except Exception as e:
+#         if show_error == "YES":
+#             notificator(
+#                 str(e) + " this shit happened in market_data.py (check_coin_price)"
+#             )
 
 
 def check_coin_price(coin_pair):
     try:
-        return exchange.fetch_ticker(coin_pair)["last"]
+        root_url = "https://api.binance.com/api/v3/ticker/24hr?symbol="
+        url = root_url + coin_pair
+        user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36"
+
+        req_session = requests.Session()
+        req_session.headers.update({"User-Agent": user_agent})
+        req_session.mount(url, HTTPAdapter(max_retries=3))
+
+        req = req_session.get(url, headers={"User-Agent": user_agent})
+        price = json.loads(req.text)["lastPrice"]
+
+        return price
 
     except Exception as e:
         if show_error == "YES":
             notificator(
-                str(e) + "this shit happened in market_data.py (check_coin_price)"
+                str(e)
+                + " this shit happened(AGAIN!) in market_data.py (check_coin_price)"
             )
 
 
