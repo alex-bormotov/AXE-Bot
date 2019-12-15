bccd062a23348817056f029943d9f802703bc9f4
Alex Bormotov <31304073+alex-bormotov@users.noreply.github.com>
2019-12-15 15:28:48 +0300

    axe-bot.com

---
+++ docs/precache-manifest.345b35a1f49e06cdb1c457878983dacd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0cddbcb6b7aa52de4763a97e941168c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a5c45dadf87f36da437",
+    "url": "/static/css/main.e3928916.chunk.css"
+  },
+  {
+    "revision": "aa03bdbd306aaef103e9",
+    "url": "/static/js/2.123d3063.chunk.js"
+  },
+  {
+    "revision": "6a5c45dadf87f36da437",
+    "url": "/static/js/main.22575e7a.chunk.js"
+  },
+  {
+    "revision": "8b3312a3346be25174e9",
+    "url": "/static/js/runtime-main.d631331d.js"
+  },
+  {
+    "revision": "3ed36b60f2302c15c4fd53a61be29721",
+    "url": "/static/media/logo.3ed36b60.svg"
+  }
+]);
